ebcf5c212e41cbec56fefe446eabf4359c230b4d
ZEN-SMS <135694111+ZEN-SMS@users.noreply.github.com>
2023-06-06 11:58:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-// Initialize markers array
+var language = 'no';
+var lang = langNO;
 
-langEN = {
-	"title_question" : "Where do you cycle in Trondheim ?",
-	"title_instructions" : "Click on the map to set an origin and then click again to set a destination. If the route created does not match to the path you use, you can delete the last point and add checkpoints along your route.",
-	"title_file_instructions" : "Or select a GPS file instead :",
-	"button_remove" : "Remove last point",
-	"button_end" : "End the route",
-	"button_send" : "Send your routes",
-	"about_you" : "About you",
-	"gender_question" : "What is your gender ?",
-	"male" : "Male",
-	"female" : "Female",
-	"pnts" : "Prefer not to say",
-	"other" : "Other :",
-	"age_question" : "What is your age ?",
-	"warning_GPX" : "Please select a .gpx file",
-	"warning_refresh" : "You already submitted your routes. If you want to add more routes, please refresh the page.",
-};
-
-langNO = {
-	"title_question" : "Hvor sykler du i Trondheim?",
-	"title_instructions" : "Klikk på kartet for å angi et utgangspunkt, og klikk deretter på nytt for å angi en destinasjon. Hvis ruten som opprettes, ikke samsvarer med ruten du bruker, kan du slette det siste punktet og legge til sjekkpunkter langs ruten.",
-	"title_file_instructions" : "Eller velg en GPS-fil i stedet:",
-	"button_remove" : "Fjern siste punkt",
-	"button_end" : "Avslutt ruten",
-	"button_send" : "Send dine ruter",
-	"about_you" : "Om deg",
-	"gender_question" : "Hvilket kjønn har du?",
-	"male" : "Mann",
-	"female" : "Kvinne",
-	"pnts" : "Foretrekker å ikke si",
-	"other" : "Annet :",
-	"age_question" : "Hvor gammel er du?",
-	"warning_GPX" : "Vennligst velg en .gpx-fil",
-	"warning_refresh" : "Du har allerede sendt inn rutene dine. Hvis du vil legge til flere ruter, vennligst oppdater siden.",
-};
-
-var language = 'en';
-var lang = langEN;
+setLanguage("no");
 
 var formID = "1FAIpQLSdHEe-PntO7HLp6-d1j8rmmjqyWxZYryNDTels3lfVU0jbuRQ"
 var formpathID = "2020612733"
@@ -105,6 +69,7 @@ function setLanguage(lg) {
 	$("#other_gender_label").text(lang.other);
 	$("#pnts_label").text(lang.pnts);
 	$("#age_question").text(lang.age_question);
+	$("label[for='file_input']").text(lang.file_input);
 }
 
 
