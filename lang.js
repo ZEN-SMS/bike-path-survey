7b687ffb9e1ff14a0a5cb89fc8e91afd13c2f491
ZEN-SMS <135694111+ZEN-SMS@users.noreply.github.com>
2023-06-30 12:01:59 +0200

    Add files via upload

--- lang.js
+++ lang.js
@@ -1,6 +1,6 @@
 langEN = {
 	"title_question" : "Where do you cycle in Trondheim ?",
-	"title_instructions" : "Click on the map to set an origin and then click again to set a destination. If the route created does not match to the path you use, you can delete the last point and add checkpoints along your route. To ensure anonymity, we ask to start drawing about 200 m from the start and end point.",
+	"title_instructions" : "Click on the map to set an origin and then click again to set a destination. If the route created does not match the path you use, you can delete the last point with \"Remove last point\" and then add a new point. When you feel that the route you have drawn is complete, click on \"End the route\" and you can fill the optional information. When you have finished, click on \"Validate this route\".  You can draw as many routes as you want. When you have finished, you can also optionally provide gender and age, and then click \"Send your routes\". To ensure anonymity, we ask to start drawing about 200 m from the start and end point.",
 	"title_file_instructions" : "Or select a GPS file instead :",
 	"button_remove" : "Remove last point",
 	"button_end" : "End the route",
@@ -12,14 +12,15 @@ langEN = {
 	"pnts" : "Prefer not to say",
 	"other" : "Other :",
 	"age_question" : "What is your age ?",
-	"file_input" : "Or select a GPS file instead",
+	"file_input" : "You may, alternatively, upload a GPS track instead. To ensure anonymity, the tracks are cropped by about 200 from end- and start points before we receive them.",
 	"warning_GPX" : "Please select a .gpx file",
 	"warning_refresh" : "You already submitted your routes. If you want to add more routes, please refresh the page.",
+	"warning_api" : "Sorry for the inconvenience, there are currently too many people responding the survey. Please try again later.",
 };
 
 langNO = {
 	"title_question" : "Hvor sykler du i Trondheim?",
-	"title_instructions" : "Klikk på kartet for å angi et utgangspunkt, og klikk deretter på nytt for å angi en destinasjon. Hvis ruten som opprettes, ikke samsvarer med ruten du bruker, kan du slette det siste punktet og legge til sjekkpunkter langs ruten. For å sikre anonymitet, ber vi deg starte markeringen inntil 200 m fra start- og endepunktet.",
+	"title_instructions" : "Klikk på kartet for å angi et startpunkt, og klikk deretter på nytt for å angi en destinasjon. Hvis den opprettede ruten ikke stemmer overens med ruten du sykler, kan du slette det siste punktet ved å klikke på «Fjern siste punkt»  for sa og legge til et nytt punkt. Når du føler at ruten du har tegnet er komplett, kan du klikke på \"Avslutt ruten\" og du vil bli spurt om å fylle ut et skjema. Når du er ferdig med å fylle ut skjemaet kan du trykke på «Valider denne ruten». Du tegne inn så mange ruter som du vil.  Når du er ferdig, kan do angi kjønn og alder, for så og klikker på \"Send dine ruter\".  For å sikre anonymitet ber vi om at du begynner å tegne ca. 200 m fra start- og sluttpunktet.",
 	"title_file_instructions" : "Eller velg en GPS-fil i stedet:",
 	"button_remove" : "Fjern siste punkt",
 	"button_end" : "Avslutt ruten",
@@ -31,7 +32,8 @@ langNO = {
 	"pnts" : "Foretrekker å ikke si",
 	"other" : "Annet :",
 	"age_question" : "Hvor gammel er du?",
-	"file_input" : "Eller velg en GPS-fil i stedet",
+	"file_input" : "Du kan også laste opp gps filer istedenfor. Disse beskjæres automatisk (før vi mottar dem) rund 200 m fra starte- og endepunktet for å sikre anonymitet.",
 	"warning_GPX" : "Vennligst velg en .gpx-fil",
 	"warning_refresh" : "Du har allerede sendt inn rutene dine. Hvis du vil legge til flere ruter, vennligst oppdater siden.",
+	"warning_api" : "Beklager uleiligheten, det er for øyeblikket for mange som svarer på undersøkelsen. Vennligst prøv igjen senere.",
 };
